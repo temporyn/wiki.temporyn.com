f240ee14d0ba2aa026d4f6ec4db47641463992c1
kkmname <kkmaddress@gmail.com>
2026-07-18 22:01:57 +0900

    feat: disable spring security

---
+++ src/main/java/com/temporyn/wiki/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.temporyn.wiki.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .logout(logout -> logout.disable());
+        return http.build();
+    }
+}
